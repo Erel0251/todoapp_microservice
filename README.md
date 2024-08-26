b022f2a873e38b97fadb02ba5eb8bd3fbc67ecb1
Tran Duc Anh <tranducanhghu@gmail.com>
2024-08-26 20:01:42 +0700

    refactor: change config eslint and prettier

--- README.md
+++ README.md
@@ -1,21 +1,20 @@
-
 # Todo App
 
 [![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
 
 ## Table of Contents
 
-* [Technicals](#Technicals)
-* [Services](#Services)
-* [Author](#Author)
-* [Acknowledgements](#Acknowledgements)
+- [Technicals](#Technicals)
+- [Services](#Services)
+- [Author](#Author)
+- [Acknowledgements](#Acknowledgements)
 
 ## Technicals
 
-- [ ] Husky
-    - [ ] Conventional Commit
-    - [ ] Prettier
-    - [ ] Eslint
+- [X] Husky
+  - [X] Conventional Commit
+  - [X] Prettier
+  - [X] Eslint
 - [ ] Nginx
 - [ ] NestJS
 - [ ] RabbitMQ
@@ -23,19 +22,16 @@
 - [ ] Docker
 - [ ] Google Cloud
 
-
 ## Services
 
 - [ ] User Service
 - [ ] Todo Service
 - [ ] Reminder Service
 
-
 ## Author
 
 - [@Erel0251](https://www.github.com/Erel0251)
 
-
 ## Acknowledgements
 
- - [README.so](https://readme.so/)
+- [README.so](https://readme.so/)
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,56 +1,30 @@
-import typescriptEslint/eslintPlugin from "@typescript-eslint/eslint-plugin";
-import unusedImports from "eslint-plugin-unused-imports";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["**/.eslintrc.js"],
-}, ...compat.extends("plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"), {
-    plugins: {
-        "@typescript-eslint": typescriptEslint/eslintPlugin,
-        "unused-imports": unusedImports,
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.jest,
-        },
-
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "module",
-
-        parserOptions: {
-            project: "tsconfig.json",
-            tsconfigRootDir: "/home/erel/Personal/nest-ts/todoapp_microservice",
-        },
-    },
-
+export default [
+  ...compat.extends('eslint:recommended', 'prettier'),
+  {
     rules: {
-        "@typescript-eslint/interface-name-prefix": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "unused-imports/no-unused-imports": "error",
-
-        "unused-imports/no-unused-vars": ["warn", {
-            vars: "all",
-            varsIgnorePattern: "^_",
-            args: "after-used",
-            argsIgnorePattern: "^_",
-        }],
+      'no-console': 'warn',
+      semi: ['error', 'always'],
     },
-}];
+    files: [
+      '**/*.js',
+      '**/*.mjs',
+      '**/*.cjs',
+      '**/*.jsx',
+      '**/*.ts',
+      '**/*.tsx',
+    ],
+  },
+];
